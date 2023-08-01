3c033a245341b962fbe0d08ce2b09d60617ba0b0
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-08-01 02:12:25 -0300

    feat: login page done

---
+++ pages/login.jsx
@@ -0,0 +1,16 @@
+import { signIn } from "next-auth/react";
+
+function Login() {
+  return (
+    <div className="flex flex-col gap-10 items-center justify-center bg-black min-h-screen w-full">
+      <img className="w-52 h-52" src="https://i.imgur.com/fPuEa9V.png" alt="" />
+      <button className="bg-[#18D860] text-white p-5 font-bold text-lg rounded-full"
+        onClick={() => signIn('spotify', { callbackUrl: "/" })}
+      >
+        Login with Spotify
+      </button>
+    </div >
+  )
+}
+
+export default Login;
