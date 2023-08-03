19389e170f479c72278d790ef14eafa9b8966517
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-08-03 20:37:12 -0300

    feat: useSpotfy custom hook for get API data

---
+++ hooks/useSpotify.js
@@ -0,0 +1,26 @@
+import { signIn } from 'next-auth/react';
+import React, { useEffect } from 'react'
+import SpotifyWebApi from 'spotify-web-api-node';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_SECRET,
+});
+
+function useSpotify() {
+  const { data: session, status } = useSpotify();
+
+  useEffect(() => {
+    if (session) {
+      // If refresh access token attemt fails, redirect user to login page
+      if (session.error === 'RefreshAccessTokenError') {
+        signIn();
+      }
+      spotifyApi.setAccessToken(session.user.accessToken);
+    }
+  }, [session])
+
+  return spotifyApi;
+}
+
+export default useSpotify
